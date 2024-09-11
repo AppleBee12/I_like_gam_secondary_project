c3816b9e954ea98dcbf6338b754ff58b7ee009ae
AppleBee12 <mieldeluna@naver.com>
2024-09-11 16:17:57 +0900

    🔥 remove:main2 삭제 및 index와 파일 합치기

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 //section01
 
+//section3
+
+$('.sec3_card').mouseenter(
+  function () {
+    $(this).css('background-image', function (_, current) {
+      return current.replace('.jpg', '.gif');
+    });
+  });
+$('.sec3_card').mouseleave(
+  function () {
+    $(this).css('background-image', function (_, current) {
+      return current.replace('.gif', '.jpg');
+    });
+  }
+);
+
+//section3 fin
