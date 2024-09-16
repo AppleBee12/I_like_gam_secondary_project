eacf57444e193736a25f991f3722db34ba06edcb
chaerimjo-web <chaerim010710@gmail.com>
2024-09-16 16:09:51 +0900

    🔨 chore: slide1 완료

--- newscenter/js/main.js
+++ newscenter/js/main.js
@@ -1,6 +1,14 @@
+const slideBtn = $('.slide_btn');
+const nextBtn = $('.swiper-button-next');
+const prevBtn = $('.swiper-button-prev');
+
+
 const swiper = new Swiper('.swiper',{
     direction: 'horizontal',
     loop: true,
+    // autoplay: {
+    //     delay: 3000
+    // },
 
     navigation: {
         nextEl:'.swiper-button-next',
@@ -8,5 +16,18 @@ const swiper = new Swiper('.swiper',{
     },
     scrollbar: {
         el: '.swiper-scrollbar',
-      },
+      }
+});
+
+nextBtn.mouseenter(function(){
+    $('.swiper-button-next').addClass('active');
+});
+prevBtn.mouseenter(function(){
+    $('.swiper-button-prev').addClass('active');
+});
+nextBtn.mouseleave(function(){
+    $('.swiper-button-next').removeClass('active');
+});
+prevBtn.mouseleave(function(){
+    $('.swiper-button-prev').removeClass('active');
 });
