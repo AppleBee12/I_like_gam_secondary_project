5c1dea46d6cd09df2b6664913f83c5412e65cce7
Naseungil <sktmddlf505@naver.com>
2024-09-19 16:40:33 +0900

    ✨ feat:  symbol_exam 효과 적용

--- CSR/js/main.js
+++ CSR/js/main.js
@@ -4,10 +4,10 @@
   let slideWidth = $slideItems.outerWidth(true); 
   let slideCount = $slideItems.length;
   let gap=10;
-  let slideWrapper = $('.slide_wrapper')
-  
+  let slideWrapper = $('.slide_wrapper');
+  let symbolOST=$('.symbol').offset().top;
+  console.log(symbolOST);
 
-  slideWrapper.css
 
   $slides.append($slideItems.clone());
 
@@ -29,4 +29,34 @@ $slides.mouseenter(function(){
 })
 $slides.mouseleave(function(){
   infiniteSlide();
+})
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>symbolOST-1000){
+    $('.sec3_title').addClass('active')
+  }else{
+    $('.sec3_title').removeClass('active');
+  }
+})
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>$('.symbol_exam p:nth-child(1)').offset().top-900){
+    $('.symbol_exam p:nth-child(1)').addClass('active')
+  }else{
+    $('.symbol_exam p:nth-child(1)').removeClass('active');
+  }
+})
+$(window).scroll(function(){
+  if($(this).scrollTop()>$('.symbol_exam p:nth-child(2)').offset().top-900){
+    $('.symbol_exam p:nth-child(2)').addClass('active')
+  }else{
+    $('.symbol_exam p:nth-child(2)').removeClass('active');
+  }
+})
+$(window).scroll(function(){
+  if($(this).scrollTop()>$('.symbol_exam p:nth-child(3)').offset().top-900){
+    $('.symbol_exam p:nth-child(3)').addClass('active')
+  }else{
+    $('.symbol_exam p:nth-child(3)').removeClass('active');
+  }
 })
