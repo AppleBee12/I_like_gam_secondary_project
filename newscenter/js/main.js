2d80ddbd515e0b1c0b1cc33feee39ee8d7adc75b
chaerimjo-web <chaerim010710@gmail.com>
2024-09-19 11:49:08 +0900

    ♻️ refactor: slide 반응형 수정

--- newscenter/js/main.js
+++ newscenter/js/main.js
@@ -6,10 +6,10 @@ const prevBtn = $('.swiper-button-prev');
 const swiper = new Swiper('.swiper',{
     direction: 'horizontal',
     loop: true,
-    autoplay: {
-        delay: 4000
+    // autoplay: {
+    //     delay: 4000
         
-    },
+    // },
 
     navigation: {
         nextEl:'.swiper-button-next',
