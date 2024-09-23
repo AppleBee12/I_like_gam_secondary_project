0179d48dc7fc8459f850a379410757bbc4c08f42
chaerimjo-web <chaerim010710@gmail.com>
2024-09-24 04:38:27 +0900

    ✨ feat: 검색기능 추가

--- js/newscenter.js
+++ js/newscenter.js
@@ -3,33 +3,33 @@ const nextBtn = $('.swiper-button-next');
 const prevBtn = $('.swiper-button-prev');
 
 
-const swiper = new Swiper('.swiper',{
+const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     // autoplay: {
     //     delay: 4000
-        
+
     // },
 
     navigation: {
-        nextEl:'.swiper-button-next',
-        prevEl:'.swiper-button-prev'
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
     },
     scrollbar: {
         el: '.swiper-scrollbar',
-      }
+    }
 });
 
-nextBtn.mouseenter(function(){
+nextBtn.mouseenter(function () {
     $('.swiper-button-next').addClass('active');
 });
-prevBtn.mouseenter(function(){
+prevBtn.mouseenter(function () {
     $('.swiper-button-prev').addClass('active');
 });
-nextBtn.mouseleave(function(){
+nextBtn.mouseleave(function () {
     $('.swiper-button-next').removeClass('active');
 });
-prevBtn.mouseleave(function(){
+prevBtn.mouseleave(function () {
     $('.swiper-button-prev').removeClass('active');
 });
 
@@ -39,31 +39,64 @@ const tab_active = $('.tab.active');
 $(function ($) {
     $(".tab-content").eq(0).show(0);
     $(".tabs .tab").click(function () {
-      var idx = $(this).index();
-      $(".tab-content").hide();
-      $(".tab-content").eq(idx).show();
-      $('.tab.active').removeClass('active');
-      $(this).addClass('active');
+        var idx = $(this).index();
+        $(".tab-content").hide();
+        $(".tab-content").eq(idx).show();
+        $('.tab.active').removeClass('active');
+        $(this).addClass('active');
 
     });
-  }); 
+});
 
-  $(window).resize(function(){
-    if($(window).width()<768){
+$(window).resize(function () {
+    if ($(window).width() < 768) {
         $('.tab-wrapper').slick({
-            slidesToShow: 1,        
-            slidesToScroll: 1,      
-            // autoplay: true,         
-            // autoplaySpeed: 4000,    
-            infinite: true 
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,         
+            autoplaySpeed: 4000,    
+            infinite: true
         });
-    } else{
+    } else {
         $('.tab-wrapper').slick('unslick');            
     }
 });
 $(window).trigger('resize');
 
 if ('ontouchstart' in window || navigator.maxTouchPoints) {
-    // 터치 장치인 경우 호버 관련 클래스를 제거하여 호버 기능 비활성화
     $('.swiper-tab').off('mouseenter mouseleave');
 }
+
+const searchInput = $('input[type="search"]');
+const tabList = $('.tab-wrapper');
+const tabListItem = $('.swiper-tab');
+const captions = $('.tab_title p:first-child');
+const dnone = 'd-none';
+
+console.log(captions);
+
+const captionArr = [];
+let counter = 0;
+captions.each(function () {
+    captionArr.push({
+        id: counter++,
+        text: $(this).text()  // jQuery의 .text() 메서드를 사용하여 텍스트를 가져옴
+    });
+});
+console.log(captionArr);
+
+
+searchInput.on('input', function () {
+    let keywords = $(this).val();
+
+    tabListItem.addClass(dnone);
+
+    let filteredArr = captionArr.filter(caption => caption.text.includes(keywords));
+    console.log(filteredArr);
+
+    filteredArr.forEach(function (item) {
+        $(tabListItem[item.id]).removeClass(dnone);
+    });
+});
+
+
